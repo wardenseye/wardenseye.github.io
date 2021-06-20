03a5331ed2a92453d3a14a5627b241c81aeb1228
XSlizeMoon <XSlizeMoon@yahoo.com>
2021-06-21 00:26:04 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
+(function(){
 $.getJSON("https://api.github.com/gists/9bcfedae0653364aad9e43c7882433f2", function (result) {
     if(!result || !result.files || !result.files["data.json"]) return;
     result = JSON.parse(result.files["data.json"].content);
         
     //Insert stats.
     $("#statss").text(numberWithCommas(result.s));
-})
+})})();
