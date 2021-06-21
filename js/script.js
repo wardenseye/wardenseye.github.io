fd755c96cc2a7e255694d1061ed6e1a8ed3e91ed
XSlizeMoon <XSlizeMoon@yahoo.com>
2021-06-21 15:59:23 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ $.getJSON("https://api.github.com/gists/9bcfedae0653364aad9e43c7882433f2", funct
     
     //Insert stats.
     $("#statss").text(numberWithCommas(result.s));
+    $("#statsc").text(numberWithCommas(result.c));
 })
 
 function numberWithCommas(x) {
